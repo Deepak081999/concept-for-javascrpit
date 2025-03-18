7a028e0db092caa100b27b418e3f8aabef54b516
Deepak saini <ys6727431@gmail.com>
2025-03-18 20:01:06 +0530

    add

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,35 +1,22 @@
-const accountId = 144553;
-let accountEmail = "Deepak@google.com";
-var accountPassword = "12345";
-accountCity = "Jaipur";
+const accountId = 144553
+let accountEmail = "Deepak@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
 let accountState;
 
-// Updating values
-accountEmail = "Deepak@Deepak.com";
-accountPassword = "21212121";
-accountCity = "jaipur";
+// accountId = 2 // not allowed
+
+
+accountEmail = "Deepak@Deepak.com"
+accountPassword = "21212121"
+accountCity = "jaipur"
 
 console.log(accountId);
-console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
 
-// Function to go back to the main page
-function goBack() {
-    window.location.href = "https://deepak081999.github.io/concept-for-javascrpit/";
-}
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
 
-// Creating a back button dynamically
-const backButton = document.createElement("button");
-backButton.innerText = "🔙 Go Back";
-backButton.onclick = goBack;
-backButton.style.position = "fixed";
-backButton.style.bottom = "20px";
-backButton.style.right = "20px";
-backButton.style.padding = "10px";
-backButton.style.fontSize = "16px";
-backButton.style.cursor = "pointer";
-backButton.style.backgroundColor = "#007bff";
-backButton.style.color = "#fff";
-backButton.style.border = "none";
-backButton.style.borderRadius = "5px";
 
-document.body.appendChild(backButton);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
