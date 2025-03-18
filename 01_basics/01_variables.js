a6cf3f2ac7391a71c5352bd39b8f1b750d87a1bc
Deepak saini <ys6727431@gmail.com>
2025-03-18 19:54:26 +0530

    add

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -12,13 +12,24 @@ accountCity = "jaipur";
 console.log(accountId);
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
 
-// Function to go back to the previous page
+// Function to go back to the main page
 function goBack() {
-    window.history.back();
+    window.location.href = "https://deepak081999.github.io/concept-for-javascrpit/";
 }
 
 // Creating a back button dynamically
 const backButton = document.createElement("button");
 backButton.innerText = "🔙 Go Back";
 backButton.onclick = goBack;
+backButton.style.position = "fixed";
+backButton.style.bottom = "20px";
+backButton.style.right = "20px";
+backButton.style.padding = "10px";
+backButton.style.fontSize = "16px";
+backButton.style.cursor = "pointer";
+backButton.style.backgroundColor = "#007bff";
+backButton.style.color = "#fff";
+backButton.style.border = "none";
+backButton.style.borderRadius = "5px";
+
 document.body.appendChild(backButton);
